731852cd1b7de9c1f2e78649bca8a4df9d8c28bf
morsals1 <157448960+morsals1@users.noreply.github.com>
2025-02-14 11:10:05 +0500

    Add files via upload

---
+++ LabProcess12psevdo/LabProcess12psevdo.cpp
@@ -0,0 +1,49 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+
+int main() {
+    const char* file = "C:\\Users\\st310-11\\Desktop\\меньшиков ПР-32\\LabProcess-main\\LabProcess12\\race.txt";
+
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+    SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
+
+    HANDLE hFile = CreateFileA(file, GENERIC_READ, FILE_SHARE_READ,
+        &sa, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cerr << "Ошибка открытия файла: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    HANDLE hpsFile = GetCurrentProcess();
+
+    if (!DuplicateHandle(GetCurrentProcess(), hFile, GetCurrentProcess(), &hpsFile,
+        0, TRUE, DUPLICATE_SAME_ACCESS)) {
+        std::cerr << "Ошибка дублирования дескриптора: " << GetLastError() << std::endl;
+        CloseHandle(hFile);
+        return 1;
+    }
+
+    wchar_t cmdline[256];
+    swprintf_s(cmdline, L"C:\\Users\\st310-11\\Desktop\\меньшиков ПР-32\\LabProcess-main\\LabProcess12child\\x64\\Debug\\LabProcess12child.exe %d", (int)hpsFile);
+
+    if (!CreateProcess(NULL, cmdline, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        std::cerr << "Ошибка создания процесса: " << GetLastError() << std::endl;
+        CloseHandle(hFile);
+        CloseHandle(hpsFile);
+        return 1;
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    std::cout << "Дочерний процесс завершен. Нажмите любую клавишу для выхода.\n";
+    _getch();
+
+    CloseHandle(hFile);
+    CloseHandle(hpsFile);
+    return 0;
+}
